e96e12b67a27484c8fe8c2a646add4f401c00e36
Jerry Zhao <jerryzhao173985@gmail.com>
2025-06-24 14:52:58 +0100

    Initial Commit

---
+++ MLGit/MLGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLGitApp.swift
+//  MLGit
+//
+//  Created by jerry on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MLGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
